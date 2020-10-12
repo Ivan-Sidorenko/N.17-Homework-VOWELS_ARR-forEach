22c240d703d8f0dbe39e9c172aee617a1aca282f
IvanSidorenko <ivan.a.sidorenko@gmail.com>
2020-10-12 04:31:14 +0300

    update

--- js/forEach.js
+++ js/forEach.js
@@ -2,13 +2,13 @@
 
 var simpleString = prompt("Введите строку");
 
-function countVowels() {
+function countVowels(text) {
   var vowelSet={"а":true, "е":true, "ё":true, 
                 "и":true, "о":true, "у":true, 
                 "ы":true, "э":true, "ю":true, "я":true};
   var counter = 0;
 
-  var sS = simpleString.toLowerCase().split("")
+  var sS = text.toLowerCase().split("")
   sS.forEach(function(matchCheck){
     if (matchCheck in vowelSet){
       counter++;
@@ -17,4 +17,4 @@ function countVowels() {
   return counter;
 }
 
-alert ("Количество гласных: "+countVowels());
+alert ("Количество гласных: "+countVowels(simpleString));
